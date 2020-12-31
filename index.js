28e960e479e3a633252ced4cdc42be61dbf8f999
psick22 <kgirok@gmail.com>
2020-12-31 18:16:04 +0900

    Add bodyparser & post request

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const { User } = require("./models/user");
 
 const mongoose = require("mongoose");
 mongoose
@@ -18,6 +24,15 @@ mongoose
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/register", (req, res) => {
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({ success: true });
+  });
 });
 
 app.listen(port, () => {
